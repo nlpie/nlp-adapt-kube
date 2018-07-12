00895cce0431d23960ec9add4959aeeb2f951bf3
Greg M. Silverman <gms@umn.edu>
2018-07-11 20:18:38 -0500

    cleanup directory structure

--- kube/ctakes/required/scripts/run_ctakes.sh
+++ kube/ctakes/required/scripts/run_ctakes.sh
@@ -20,5 +20,5 @@ popd
 
 ##### Create cTAKES NLP-TAB profile and upload archive #####
 CTAKES_META='{"systemName":"cTAKES", "systemDescription":"cTAKES annotation engine", "instance":"default"}'
-RESPONSE=$(echo $CTAKES_META | curl -sS -d @- http://localhost:9200/_nlptab-systemindexmeta)
-curl -sS --data-binary @$CTAKES_OUT.zip -H 'Content-Type: application/zip' "http://localhost:9200/_nlptab-systemindex?instance=default&index=$(echo $RESPONSE | jq -r .index)&useXCas=false"
+RESPONSE=$(echo $CTAKES_META | curl -sS -d @- http://192.168.99.100:31345/_nlptab-systemindexmeta)
+curl -sS --data-binary @$CTAKES_OUT.zip -H 'Content-Type: application/zip' "http://192.168.99.100:31345/_nlptab-systemindex?instance=default&index=$(echo $RESPONSE | jq -r .index)&useXCas=false"
