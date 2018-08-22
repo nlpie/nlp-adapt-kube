d294deb314a12d5d42eac138bbaefb2c7b5f8223
Greg Silverman <gms@umn.edu>
2018-08-22 17:27:57 -0500

    clean up

--- docker/amicus/required/scripts/run_amicus.sh
+++ docker/amicus/required/scripts/run_amicus.sh
@@ -4,5 +4,5 @@
 if [ -f /data/in/export.yml ]; then
     java -jar /usr/share/amicus/amicus.jar /data/in/export.yml
 else
-    java -jar /usr/share/amicus.jar /home/ubuntu/amicus/nlpie/merge_concepts.yml
+    java -jar /usr/share/amicus/amicus.jar /usr/share/amicus/nlpie/merge_concepts.yml
 fi
