6e81fc0af89dcddcd082c1590ee0f2da52d3e0b1
Greg Silverman <Greg Silverman>
2019-03-21 20:48:29 -0500

    splitting up mm docker

--- scripts/save_tar.sh
+++ scripts/save_tar.sh
@@ -1,5 +1,5 @@
 docker save ahc-nlpie-docker.artifactory.umn.edu/metamap > ~/development/nlp/nlpie/docker_export_files/metamap.tar
 docker save ahc-nlpie-docker.artifactory.umn.edu/biomedicus > ~/development/nlp/nlpie/docker_export_files/biomedicus.tar
-docker save ahc-nlpie-docker.artifactory.umn.edu/ctakes > ~/development/nlp/nlpie/docker_export_files/ctakes.tar
+#docker save ahc-nlpie-docker.artifactory.umn.edu/ctakes > ~/development/nlp/nlpie/docker_export_files/ctakes.tar
 docker save ahc-nlpie-docker.artifactory.umn.edu/clamp > ~/development/nlp/nlpie/docker_export_files/clamp.tar
 
