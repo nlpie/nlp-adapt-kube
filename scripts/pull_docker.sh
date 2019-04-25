ba802c486c89e5374d1e9e6cd6dd953e7301023e
Greg Silverman <Greg Silverman>
2019-04-25 13:21:18 -0500

    odds and ends

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -4,7 +4,7 @@
 
 #eval $(minikube docker-env)
 
-docker login ahc-nlpie-docker.artifactory.umn.edu/biomedicus 
+docker login ahc-nlpie-docker.artifactory.umn.edu
 docker images
 
 echo "Begin uploading biomedicus image..."
@@ -12,19 +12,30 @@ docker pull ahc-nlpie-docker.artifactory.umn.edu/biomedicus
 echo "end biomedicus"
 
 echo "Begin uploading clamp image..."
-docker pull ahc-nlpie-docker.artifactory.umn.edu/clamp 
+#docker pull ahc-nlpie-docker.artifactory.umn.edu/clamp 
 echo "end clamp"
 
 echo "Begin uploading ctakes image..."
-docker pull ahc-nlpie-docker.artifactory.umn.edu/ctakes 
+#docker pull ahc-nlpie-docker.artifactory.umn.edu/ctakes 
+echo "end ctakes"
+
+echo "Begin uploading amq image..."
+docker pull ahc-nlpie-docker.artifactory.umn.edu/amq 
+echo "end ctakes"
+
+echo "Begin uploading cpc image..."
+docker pull ahc-nlpie-docker.artifactory.umn.edu/cpc
 echo "end ctakes"
 
 echo Begin "uploading metamap image..."
-docker pull ahc-nlpie-docker.artifactory.umn.edu/metamap 
+docker pull ahc-nlpie-docker.artifactory.umn.edu/mmserver	
+docker pull ahc-nlpie-docker.artifactory.umn.edu/mmannotator
+docker pull ahc-nlpie-docker.artifactory.umn.edu/wsd
+docker pull ahc-nlpie-docker.artifactory.umn.edu/medpost 
 echo "end metamap"
 
 echo "Begin uploading amicus image..."
-docker pull ahc-nlpie-docker.artifactory.umn.edu/amicus 
+#docker pull ahc-nlpie-docker.artifactory.umn.edu/amicus 
 echo "end amicus"
 
 
