7d2c3a51f6727340d55192f0b1412184d9c0879f
Greg Silverman <Greg Silverman>
2019-05-06 15:57:02 -0500

    ctakes and clamp testing

--- docker/uima-as/clamp/required/clamp-as/bin/deployClampPipeline.sh
+++ docker/uima-as/clamp/required/clamp-as/bin/deployClampPipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CLAMP_HOME=${CLAMP_HOME:-"/usr/share/clamp"}
 
-clampjar=$CLAMP_HOME/bin/clamp-nlp-1.5.1-jar-with-dependencies.jar
+clampjar=$CLAMP_HOME/bin/clamp-nlp-1.6.0-jar-with-dependencies.jar
 pipeline=$CLAMP_HOME/pipeline/clamp-ner.pipeline.jar
 umls_index=$CLAMP_HOME/resource/umls_index/
 config=$CLAMP_HOME/config/ServiceConfig.groovy
