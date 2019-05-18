c23eeb5208a52b204590b1b2ce842bcc7657bb5b
Raymond Finzel <finze006@umn.edu>
2019-05-18 10:48:08 -0500

    biomedicus client that serializes xmi to database CLOB

--- docker/client/required/scripts/biomedicus_client.groovy
+++ docker/client/required/scripts/biomedicus_client.groovy
@@ -1,4 +1,5 @@
 import java.util.concurrent.TimeUnit
+import java.io.ByteArrayOutputStream
 
 import groovy.io.FileType
 import groovy.sql.Sql
@@ -25,12 +26,15 @@ import edu.umn.nlpengine.Artifact
 import edu.umn.biomedicus.types.*
 
 def env = System.getenv();
-def group = new DefaultPGroup(4);
-def inDataSource = new BasicDataSource();
-inDataSource.setUrl(env["NLPADAPT_INPUT_DATASOURCE_URI"]);
-def outDataSource = new BasicDataSource();
-outDataSource.setUrl(env["NLPADAPT_OUTPUT_DATASOURCE_URI"]);
+def group = new DefaultPGroup(8);
+def dataSource = new BasicDataSource()
+dataSource.setPoolPreparedStatements(true)
+dataSource.setMaxTotal(5)
+dataSource.setUrl(env["NLPADAPT_DATASOURCE_URI"])
+dataSource.setUsername(env["NLPADAPT_DATASOURCE_USERNAME"])
+dataSource.setPassword(env["NLPADAPT_DATASOURCE_PASSWORD"])
 
+def outputQueue = new DataflowQueue();
 
 def getUimaPipelineClient(uri, endpoint, callback, poolsize) {
     def pipeline = new BaseUIMAAsynchronousEngine_impl()
@@ -45,24 +49,44 @@ def getUimaPipelineClient(uri, endpoint, callback, poolsize) {
 final def biomedicusPipeline = getUimaPipelineClient(
     env["NLPADAPT_BROKER_URI"],
     "nlpadapt.biomedicus.outbound",
-    new BiomedicusCallbackListener(outDataSource),
+    new BiomedicusCallbackListener(outputQueue),
     16
 );
 
 final def biomedicusArtificer = group.reactor {
     def cas = biomedicusPipeline.getCAS()
-    def note = it.rtf2plain
-    def source_note_id = it.id // for toy sqlite schema
+    def note = it.rtf2plain?.characterStream?.text
+    def source_note_id = it.note_id // for toy sqlite schema
     def to_process = cas.createView("OriginalDocument")
     to_process.setDocumentText(note)
     UimaAdapters.createArtifact(cas, null, source_note_id.toString())
     reply cas
 }
 
-class BiomedicusCallbackListener extends UimaAsBaseCallbackListener {
-  BasicDataSource output;
+final def biomedicusDatabaseWrite = group.reactor { data ->
+  def sql = Sql.newInstance(dataSource);
+  if(data.b9 == 'P'){
+    data.xmi = data.xmi?.toString()
+    // sql.withTransaction{
+    //   sql.withBatch(100, "INSERT INTO nlp_sandbox.u01_detected_item(item_type, item, note_id, engine_id, begin, end, negated, text) VALUES (:item_type, :item, :note_id, :engine_id, :begin, :end, :negated, :text)"){ ps ->
+    // 	for(i in data.items){
+    // 	  ps.addBatch(i)
+    // 	}
+    //   }
+    // }    
+    sql.executeUpdate "UPDATE u01_tmp SET b9=$data.b9, xmi=$data.xmi WHERE note_id=$data.note_id"
+    reply "SUCCESS: $data.note_id"
+  } else {
+    sql.executeUpdate "UPDATE u01_tmp SET b9=$data.b9, error=$data.error WHERE note_id=$data.note_id"
+    reply "ERROR:   $data.note_id"
+  }
+  sql.close()
+};
 
-  BiomedicusCallbackListener(BasicDataSource output){
+class BiomedicusCallbackListener extends UimaAsBaseCallbackListener {
+  DataflowQueue output;
+
+  BiomedicusCallbackListener(DataflowQueue output){
 	this.output = output
     }
 
@@ -71,50 +95,45 @@ class BiomedicusCallbackListener extends UimaAsBaseCallbackListener {
 
 	Type type = aCas.getTypeSystem().getType("ArtifactID");
 	Feature documentId = type.getFeatureByBaseName("artifactID");
-	
-	// Type conceptType = aCas.getTypeSystem().getType("uima.tcas.Annotation");
-	//Feature cui = type.getFeatureByBaseName("cui");
-	
+		
 	Integer source_note_id = aCas.getView("metadata")
 	.getIndexRepository()
 	.getAllIndexedFS(type)
 	.next()
 	.getStringValue(documentId) as Integer;
-
-	// CASArtifact response = UimaAdapters.getArtifact(aCas.getView("Analysis"), null)
-	// def b9 = 'P';
-	// def sql = Sql.newInstance(this.output);
-	// sql.executeUpdate "UPDATE source_note SET b9=$b9 WHERE id=$source_note_id";
-	//println "callback"
-	// for(FeatureStructure concept: aCas.getView("Analysis").getIndexRepository().getAllIndexedFS(conceptType)){
-	//   for(Feature f: conceptType.getFeatures()){
-	//     println concept.getStringValue(f)
-	//   }
-	// }
-	//println aCas.getView("Analysis").getIndexRepository().getAllIndexedFS(conceptType)
-	// for(Type t: aCas.getTypeSystem().getTypeIterator()){
-	//   println t.getName()
-	// }
-	//XmlCasSerializer.serialize(aCas, System.out)
-	//println aCas.getView("Analysis")
-	// for(CAS c: aCas.getViewIterator()){
-	//   for(FeatureStructure fs: c.getIndexRepository().getAllIndexedFS(c.getJCas().getCasType(TOP.type))){
-	//     println fs.getType().getName()
-	//   }
-	// }
-	XmlCasSerializer.serialize(aCas, System.out)
-	println "\n***************************************************"
+	
+	if(!aStatus.isException()){
+	  ByteArrayOutputStream xmi = new ByteArrayOutputStream();
+	  XmlCasSerializer.serialize(aCas, xmi)
+	  def process_results = [note_id:source_note_id, b9:'P', xmi:xmi]
+	  this.output << process_results
+	} else {
+	  def process_results = [note_id:source_note_id, b9:'E', error:aStatus.getStatusMessage().take(3999)]
+	  this.output << process_results
+	}
+	
     }
 }
 
-def in_db = Sql.newInstance(inDataSource);
-in_db.withStatement { stmt ->
-  stmt.setFetchSize(25)
+outputQueue.wheneverBound {
+  group.actor{
+    biomedicusDatabaseWrite.send outputQueue.val
+    react {
+      println "$it"
+    }
+  }
 }
 
+
 while(true){
-  def query = "SELECT id, rtf2plain FROM source_note WHERE rtf2plain IS NOT NULL AND b9='U' limit 1000";
-  in_db.eachRow(query){ row ->
+  def in_db = Sql.newInstance(dataSource);
+  in_db.withStatement { stmt ->
+    stmt.setFetchSize(50)
+  }
+  
+  in_db.eachRow("SELECT note_id, rtf2plain FROM nlp_sandbox.u01_tmp WHERE rtf2plain IS NOT NULL AND b9 IN ('U', 'R') AND rownum<=1000"){ row ->
     biomedicusPipeline.sendCAS(biomedicusArtificer.sendAndWait(row));
   }
+  
+  in_db.close()
 }
