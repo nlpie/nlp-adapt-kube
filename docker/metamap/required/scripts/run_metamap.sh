de54bf3fa6f29078c5241767a2bc40807af03fe0
Greg Silverman <Greg Silverman>
2019-06-12 13:58:24 -0500

    fix run command

--- docker/metamap/required/scripts/run_metamap.sh
+++ docker/metamap/required/scripts/run_metamap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export DATA_DIRECTORY=/data
 export METAMAP_OUT=$DATA_DIRECTORY/metamap_out
@@ -28,54 +28,11 @@ done
 
 mmserver &
 	
-
-# TODO 
-#source ./setup_uima.sh
-
-BASEDIR=/usr/share/public_mm
-export BASEDIR
-
-MM_API_UIMA=${BASEDIR}/src/uima
-export MM_API_UIMA
-JAVAAPI=${BASEDIR}/src/javaapi/dist/MetaMapApi.jar
-export JAVAAPI
-PROLOGBEANS=${BASEDIR}/src/javaapi/dist/prologbeans.jar
-export PROLOGBEANS
-
-# Apache UIMA classes
-UIMA_HOME=/usr/share/uima
-export UIMA_HOME
-UIMA_CLASSPATH=\
-${UIMA_HOME}/examples/resources:\
-${UIMA_HOME}/lib/uima-core.jar:\
-${UIMA_HOME}/lib/uima-document-annotation.jar:\
-${UIMA_HOME}/lib/uima-cpe.jar:\
-${UIMA_HOME}/lib/uima-tools.jar:\
-${UIMA_HOME}/lib/uima-examples.jar:\
-${UIMA_HOME}/lib/uima-adapter-soap.jar:\
-${UIMA_HOME}/lib/uima-adapter-vinci.jar:\
-${UIMA_HOME}/lib/jVinci.jar  
-export UIMA_CLASSPATH
-
-# classpath
-CLASSPATH=\
-${CLASSPATH}:\
-${MM_API_UIMA}/classes:\
-${MM_API_UIMA}/desc:\
-${BASEDIR}/nlpie:\
-${MM_API_UIMA}/lib/metamap-api-uima.jar:\
-${UIMA_CLASSPATH}:\
-${JAVAAPI}:\
-${PROLOGBEANS}
-export CLASSPATH
-
-PATH=$PATH:${UIMA_HOME}/bin
-export PATH
-# end TODO
-
-runCPE.sh $METAMAP_HOME/nlpie/MetaMapCPM_nlpie.xml
+source /usr/share/public_mm/src/uima/bin/setup_uima.sh
 
 mkdir -p $METAMAP_OUT
+
+runCPE.sh $METAMAP_HOME/nlpie/MetaMapCPM_nlpie.xml
 
 ##### Create Archive for NLP-TAB #####
 cp $METAMAP_HOME/nlpie/CombinedTypeSystem.xml $METAMAP_OUT/TypeSystem.xml
