44f9c0fe2b0ce237eb29fa594c3810bca6a85bb6
Greg Silverman <gms@umn.edu>
2019-09-24 20:25:35 -0500

    issue with ctakes efault piper; switch GC

--- docker/cpm/clamp/required/nlpie/run_nlpie_pipeline.sh
+++ docker/cpm/clamp/required/nlpie/run_nlpie_pipeline.sh
@@ -11,11 +11,12 @@ umlsPass=$umlsPass
 input=$DATA_IN
 output=$CLAMP_OUT
 
+
 clampbin="$CLAMP_HOME/bin/clamp-nlp-1.6.0-jar-with-dependencies.jar"
 pipeline="$CLAMP_HOME/pipeline/clamp-ner-attribute.pipeline.jar"
 umlsIndex="$CLAMP_HOME/resource/umls_index/"
 
-java -DCLAMPLicenceFile="$CLAMP_HOME/CLAMP.LICENSE" -Xmx3g -cp $clampbin edu.uth.clamp.nlp.main.PipelineMain \
+java -XX:+UseConcMarkSweepGC -DCLAMPLicenceFile="$CLAMP_HOME/CLAMP.LICENSE" -Xmx5g -cp $clampbin edu.uth.clamp.nlp.main.PipelineMain \
     -i $input \
     -o $output \
     -p $pipeline \
